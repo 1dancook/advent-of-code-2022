70761321c17fb05692f3241c16fe20ba3fb0aaa1
Dan Cook <cook.r.dan@gmail.com>
2022-12-02 21:28:34 +0900

    add template

---
+++ template.py
@@ -0,0 +1,26 @@
+from get_input import *
+
+DAY = 2
+
+def part1(data):
+    ...
+
+def part2(data):
+    ...
+
+
+# Tests
+sample = get_sample_input(DAY)
+part1_result = None
+part2_result = None
+assert part1(sample) == part1_result
+assert part2(sample) == part2_result
+
+
+data = get_input(DAY)
+print("Part 1 Result")
+print(part1(data))
+
+print("Part 2 Result")
+print(part2(data))
+
