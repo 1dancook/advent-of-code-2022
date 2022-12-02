c5a8b56ba4e5f4c271c15f8b620939389858e02c
Dan Cook <cook.r.dan@gmail.com>
2022-12-02 21:28:21 +0900

    add get_sample_input function

--- get_input.py
+++ get_input.py
@@ -1,3 +1,11 @@
-def get_input(day):
-    with open(f"input/{day}.txt", "r") as f:
-        return f.read().strip()
+# don't know if there would be two different 
+# types of data for one day or not
+def get_input(day, folder="input"):
+    try:
+        with open(f"{folder}/{day}.txt", "r") as f:
+            return f.read().strip()
+    except:
+        return ""
+
+def get_sample_input(day):
+    return get_input(day, folder="sample_input")
