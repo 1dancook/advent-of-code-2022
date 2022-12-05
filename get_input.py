65ad804905768324dfe33f02e4ed556ae43255a1
Dan Cook <cook.r.dan@gmail.com>
2022-12-05 21:26:10 +0900

    change: add strip option to get_input.py

--- get_input.py
+++ get_input.py
@@ -1,14 +1,14 @@
 # don't know if there would be two different 
 # types of data for one day or not
-def get_input(day, folder="input"):
+def get_input(day, folder="input", strip=True):
     try:
         with open(f"{folder}/{day}.txt", "r") as f:
-            return f.read().strip()
+            return f.read().strip() if strip else f.read()
     except:
         return ""
 
-def get_sample_input(day):
-    return get_input(day, folder="sample_input")
+def get_sample_input(day, strip=True):
+    return get_input(day, folder="sample_input", strip=strip)
 
 def sample_as_lines(day):
     data = get_input(day, folder="sample_input")
